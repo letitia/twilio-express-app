bcd2a2f7938cb925cbe090abb33fb162194de408
Letitia Lew <lew.letitia@gmail.com>
2017-08-13 14:38:06 -0700

    Add endpoints to make outgoing calls with a voice message

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const twilio = require('twilio');
+const config = require('./config/config');
 const VoiceResponse = twilio.twiml.VoiceResponse;
+
+const testClient = new twilio(config.twilio.test.accountSid, config.twilio.test.authToken);
+const client = new twilio(config.twilio.live.accountSid, config.twilio.live.authToken);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,6 +22,23 @@ app.post('/conference', (req, res) => {
   const twiml = new VoiceResponse();
   const dial = twiml.dial();
   dial.conference('My Conference Room 01');
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+app.post('/call', (req, res) => {
+  client.api.calls.create({
+    url: `http:\/\/${req.hostname}/voicemessage`,
+    to: config.phoneNumbers.self,
+    from: config.phoneNumbers.account
+  })
+  .then((call) => console.log(call.sid));
+});
+
+app.post('/voicemessage', (req, res) => {
+  const twiml = new VoiceResponse();
+  twiml.say({ voice: 'alice' }, 'Hi honey. Shall we get pizza now?');
+  twiml.play('http://demo.twilio.com/docs/classic.mp3');
   res.type('text/xml');
   res.send(twiml.toString());
 });
