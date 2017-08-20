43b359a44dc4e40a7d78ffb787661c9bf8461384
Letitia Lew <lew.letitia@gmail.com>
2017-08-20 12:27:56 -0700

    Use Heroku's DATABASE_URL in prod

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
 const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const config = require('../config/config').development;
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 const basename = path.basename(module.filename);
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: config.dialect
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 const db = {};
 
